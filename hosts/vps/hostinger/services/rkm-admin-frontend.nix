4d572554c46af97dfe9967b38b3797d9514cf51e
maulanasdqn <maulanasdqn@gmail.com>
2026-01-11 03:12:20 +0700

    fix: use rkm-admin-frontend nginx option

--- hosts/vps/hostinger/services/rkm-admin-frontend.nix
+++ hosts/vps/hostinger/services/rkm-admin-frontend.nix
@@ -2,14 +2,11 @@
 {
   services.rkm-admin-frontend = {
     enable = true;
-  };
-
-  services.nginx.virtualHosts."cms.rajawalikaryamulya.co.id" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://127.0.0.1:3100";
-      proxyWebsockets = true;
+    port = 3100;
+    nginx = {
+      enable = true;
+      domain = "cms.rajawalikaryamulya.co.id";
+      enableSSL = true;
     };
   };
 }
