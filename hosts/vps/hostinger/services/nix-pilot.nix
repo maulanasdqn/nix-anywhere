2c973be8d0be95fa4827bcfa4c61d6a8b5f6b163
maulanasdqn <maulanasdqn@gmail.com>
2026-02-19 13:42:39 +0700

    chore: Update nix-pilot domains to manage.msdqn.dev

--- hosts/vps/hostinger/services/nix-pilot.nix
+++ hosts/vps/hostinger/services/nix-pilot.nix
@@ -6,11 +6,20 @@
     address = "127.0.0.1";
   };
 
-  services.nginx.virtualHosts."nix-pilot.msdqn.my.id" = {
+  services.nginx.virtualHosts."manage.msdqn.dev" = {
     enableACME = true;
     forceSSL = true;
     locations."/" = {
       proxyPass = "http://127.0.0.1:3001";
+      proxyWebsockets = true;
+    };
+  };
+
+  services.nginx.virtualHosts."api-manage.msdqn.dev" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:3001/api";
       proxyWebsockets = true;
     };
   };
