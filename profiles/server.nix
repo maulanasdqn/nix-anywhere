ce826aeb0d6d629763f404b804e1d26186d91c41
maulanasdqn <maulanasdqn@gmail.com>
2026-03-04 12:25:33 +0700

    fix: disable recommendedBrotliSettings to prevent duplicate directive

--- profiles/server.nix
+++ profiles/server.nix
@@ -75,6 +75,7 @@
     recommendedOptimisation = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
+    recommendedBrotliSettings = false; # Disabled - app modules add their own brotli settings
     sslCiphers = "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384";
     appendHttpConfig = ''
       ssl_ecdh_curve X25519:secp384r1:prime256v1;
